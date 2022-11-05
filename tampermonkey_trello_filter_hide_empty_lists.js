1d57c27da3356a7f8eea1dcaee3c7110b8d4f951
Mattias Jonsson <mattias.jonsson@gmail.com>
2022-11-05 11:23:06 -0400

    Performance improvement for Trello filter

--- tampermonkey_trello_filter_hide_empty_lists.js
+++ tampermonkey_trello_filter_hide_empty_lists.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         Trello Filter: Hide Empty Lists
-// @namespace    http://trello.com/
+// @namespace    https://trello.com/
 // @version      1.0
 // @description  When using Trello's card filter feature, this script will show only lists where the filter matches at least one card. All other lists will be hidden.
 // @author       Mattias Jonsson
-// @match        https://trello.com/b/*
+// @match        *://trello.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
+(function() {
 window.addEventListener('load', function() {
     console.log({
         'loading': 'Tampermonkey Script',
@@ -16,8 +17,22 @@ window.addEventListener('load', function() {
         'description': GM_info.script.description
     });
     var $ = window.jQuery;
+
     window.setInterval( function() {
-        var isFiltered = $('.mod-filter-indicator:not(.hide)').length > 0;
+        var isFiltered = $('.js-brita-board-filter-btn-container button span[aria-label="CloseIcon"]').length > 0;
+
+        // Turn off the comment-record-button's animation
+        $('div.js-comment-record-button a').css('animation', 'none');
+
+        // Remove "smart links" that change link text to title from the target
+        $('a.atlaskit-smart-link').each( function() {
+            var link = $(this);
+            link.removeClass('atlaskit-smart-link');
+            link.empty();
+            link.text(link.prop('href'));
+        });
+
+        // Filtering?
         if ( !isFiltered && window.trelloIsFiltered === false ) {
             // Not currently filtered, and not filtered last time: All lists are already shown
             // Note: End here for performance reason
@@ -36,3 +51,4 @@ window.addEventListener('load', function() {
         window.trelloIsFiltered = isFiltered;
     }, 100 );
 }, false);
+})();
